22612b89664203c9b548cfe51c7d21624d225630
brianbeach95 <brianbeacBrianh@192.168.0.21>
2022-03-19 14:17:06 -0500

    Initial Commit

---
+++ Ind03_Beach_Brian/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ind03_Beach_Brian
+//
+//  Created by Brian Beach on 3/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
